b8f0e9b86f46e3488a5e123c678fb686b74e6329
nttnhannho <makelifeperfect@gmail.com>
2021-10-20 20:21:29 +0700

    Add bubble sort; Some MISC changes in selection sort

--- sort_algorithms/selection_sort.py
+++ sort_algorithms/selection_sort.py
@@ -1,29 +1,26 @@
 def selection_sort(sample_lst, reverse=False):
     for index, item in enumerate(sample_lst[:-1]):
-        if reverse:
-            val = max(sample_lst[index:])
-        else:
-            val = min(sample_lst[index:])
+        val = min(sample_lst[index:])
         val_index = sample_lst.index(val, index)
         if index != val_index:
             sample_lst[index], sample_lst[val_index] = sample_lst[val_index], sample_lst[index]
+
+    if reverse:
+        sample_lst.reverse()
 
 
 def selection_sort_without_minmax(sample_lst, reverse=False):
     for index, item in enumerate(sample_lst[:-1]):
-        if reverse:
-            val = sample_lst[index]
-            for subitem in sample_lst[index + 1:]:
-                if subitem > val:
-                    val = subitem
-        else:
-            val = sample_lst[index]
-            for subitem in sample_lst[index + 1:]:
-                if subitem < val:
-                    val = subitem
+        val = sample_lst[index]
+        for subitem in sample_lst[index + 1:]:
+            if subitem < val:
+                val = subitem
         val_index = sample_lst.index(val, index)
         if index != val_index:
             sample_lst[index], sample_lst[val_index] = sample_lst[val_index], sample_lst[index]
+
+    if reverse:
+        sample_lst.reverse()
 
 
 if __name__ == "__main__":
@@ -35,17 +32,29 @@ if __name__ == "__main__":
     selection_sort(sample2)
     print(sample2)
 
-    selection_sort_without_minmax(sample1)
-    print(sample1)
+    sample3 = [56, 3, 2, 78, 6, 0]
+    selection_sort(sample3, True)
+    print(sample3)
 
-    selection_sort_without_minmax(sample2)
-    print(sample2)
+    sample4 = [56, 0, 2, 2, 6, 0]
+    selection_sort(sample4, True)
+    print(sample4)
 
-    selection_sort_without_minmax(sample1, True)
-    print(sample1)
+    sample5 = [56, 3, 2, 78, 6, 0]
+    selection_sort_without_minmax(sample5)
+    print(sample5)
 
-    selection_sort_without_minmax(sample2, True)
-    print(sample2)
+    sample6 = [56, 0, 2, 2, 6, 0]
+    selection_sort_without_minmax(sample6)
+    print(sample6)
+
+    sample7 = [56, 3, 2, 78, 6, 0]
+    selection_sort_without_minmax(sample7, True)
+    print(sample7)
+
+    sample8 = [56, 0, 2, 2, 6, 0]
+    selection_sort_without_minmax(sample8, True)
+    print(sample8)
 
     nums = int(input("How many numbers you want to enter?: "))
     lst = [int(input("Enter number: ")) for num in range(nums)]
