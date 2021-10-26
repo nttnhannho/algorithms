e09a32ce969a32c7e9093b280386a6bdca2f6b54
nttnhannho <makelifeperfect@gmail.com>
2021-10-26 21:46:40 +0700

    Add shell sort

--- sort_algorithms/shell_sort.py
+++ sort_algorithms/shell_sort.py
@@ -1,12 +1,15 @@
-def insertion_sort(sample_lst, reverse=False):
+def shell_sort(sample_lst, reverse=False):
     # Time complexity = O(n^2)
     # Space complexity = O(1)
-    for index, item in enumerate(sample_lst[1:], start=1):
-        pos = index
-        while item < sample_lst[pos - 1] and pos > 0:
-            sample_lst[pos] = sample_lst[pos - 1]
-            pos -= 1
-        sample_lst[pos] = item
+    gap = len(sample_lst) // 2
+    while gap > 0:
+        for index, item in enumerate(sample_lst[gap:], start=gap):
+            pos = index
+            while pos >= gap and item < sample_lst[pos - gap]:
+                sample_lst[pos] = sample_lst[pos - gap]
+                pos -= gap
+            sample_lst[pos] = item
+        gap = gap // 2
 
     if reverse:
         sample_lst.reverse()
@@ -14,23 +17,23 @@ def insertion_sort(sample_lst, reverse=False):
 
 if __name__ == "__main__":
     sample1 = [2, 4, 3, 5, 1]
-    insertion_sort(sample1)
+    shell_sort(sample1)
     print(sample1)
 
     sample2 = [2, 2, 3, 4, 3]
-    insertion_sort(sample2)
+    shell_sort(sample2)
     print(sample2)
 
     sample3 = [2, 4, 3, 5, 1]
-    insertion_sort(sample3, True)
+    shell_sort(sample3, True)
     print(sample3)
 
     sample4 = [2, 2, 3, 4, 3]
-    insertion_sort(sample4, True)
+    shell_sort(sample4, True)
     print(sample4)
 
     nums = int(input("How many numbers you want to enter?: "))
     input_lst = [int(input("Enter number: ")) for num in range(nums)]
     print("Unsorted list", input_lst)
-    insertion_sort(input_lst)
+    shell_sort(input_lst)
     print("Ascending order:", input_lst)
