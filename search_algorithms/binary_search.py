4946e794439f7e8ec3ff3a5821c9a7830bfd8e09
nttnhannho <makelifeperfect@gmail.com>
2021-10-28 16:27:15 +0700

    Add recursive binaray search with rotated list

--- search_algorithms/binary_search.py
+++ search_algorithms/binary_search.py
@@ -1,16 +1,16 @@
 def recursive_binary_search_sorted(sample_lst, left, right, value):
     # Time complexity = O(log(n))
     # Space complexity = O(log(n))
-    if left < right:
-        mid = left + (right - left) // 2
-        mid_val = sample_lst[mid]
-        if mid_val == value:
-            return mid
-        if value < mid_val:
-            return recursive_binary_search_sorted(sample_lst, left, mid - 1, value)
-        if value > mid_val:
-            return recursive_binary_search_sorted(sample_lst, mid + 1, right, value)
-    return -1
+    if left > right:
+        return -1
+    mid = left + (right - left) // 2
+    mid_val = sample_lst[mid]
+    if mid_val == value:
+        return mid
+    if value < mid_val:
+        return recursive_binary_search_sorted(sample_lst, left, mid - 1, value)
+    if value > mid_val:
+        return recursive_binary_search_sorted(sample_lst, mid + 1, right, value)
 
 
 def iterative_binary_search_sorted(sample_lst, value):
@@ -28,6 +28,26 @@ def iterative_binary_search_sorted(sample_lst, value):
         else:
             right = mid - 1
     return -1
+
+
+def recursive_binary_search_rotated(sample_lst, left, right, value):
+    # Time complexity = O(log(n))
+    # Space complexity = O(log(n))
+    mid = (left + right) // 2
+    if left > right:
+        return -1
+    if sample_lst[mid] == value:
+        return mid
+    if sample_lst[left] <= sample_lst[mid]:
+        if sample_lst[left] <= value <= sample_lst[mid]:
+            return recursive_binary_search_rotated(sample_lst, left, mid - 1, value)
+        else:
+            return recursive_binary_search_rotated(sample_lst, mid + 1, right, value)
+    else:
+        if sample_lst[mid] <= value <= sample_lst[right]:
+            return recursive_binary_search_rotated(sample_lst, mid + 1, right, value)
+        else:
+            return recursive_binary_search_rotated(sample_lst, left, mid - 1, value)
 
 
 if __name__ == "__main__":
@@ -48,3 +68,12 @@ if __name__ == "__main__":
 
     sample6 = [1, 2, 3, 4, 5]
     print(iterative_binary_search_sorted(sample6, 100))
+
+    sample7 = [4, 5, 6, 7, 8, 9, 1, 2, 3]
+    print(recursive_binary_search_rotated(sample7, 0, len(sample7) - 1, 3))
+
+    sample8 = [4, 5, 6, 7, 8, 9, 1, 2, 3]
+    print(recursive_binary_search_rotated(sample8, 0, len(sample8) - 1, 100))
+
+    sample9 = [3, 4, 5, 6, 7, 8, 9, 1, 2]
+    print(recursive_binary_search_rotated(sample9, 0, len(sample9) - 1, 7))
